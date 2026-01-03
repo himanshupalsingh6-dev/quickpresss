ba74e32035532c5ccac7b9238fe5ebb848643748
himanshupalsingh6-dev <himanshupalsingh6@gmail.com>
2026-01-03 16:42:44 +0530

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,1 +1,7 @@
-
+const products = [
+  { id: 1, name: "Shirt", price: 10 },
+  { id: 2, name: "Pant", price: 12 },
+  { id: 3, name: "Saree", price: 40 },
+  { id: 4, name: "Woolen", price: 25 },
+  { id: 5, name: "Office Wear", price: 20 }
+];
