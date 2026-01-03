f33ca89cfa927667df8539d3645b636200e8b401
himanshupalsingh6-dev <himanshupalsingh6@gmail.com>
2026-01-03 16:44:02 +0530

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,1 +1,15 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
+function addToCart(id) {
+  const product = products.find(p => p.id === id);
+  const item = cart.find(i => i.id === id);
+
+  if (item) {
+    item.qty += 1;
+  } else {
+    cart.push({ ...product, qty: 1 });
+  }
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+  alert("Added to cart");
+}
